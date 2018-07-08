011779bc58b237f16e0b1aa0230944e9a4b03c5d
小川哲梦 <=>
2018-07-08 11:29:02 +0800

    .
    Signed-off-by: 小川哲梦 <=>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Node-learning-code
 NodeJS基础学习笔记、源码分析、代码编写
+版本：nodeJs: 8.9.0以上
+
+
+
---
+++ code/event/self.js
@@ -0,0 +1,33 @@
+var EventEmitter = require('./EventEmitter');
+var util = require('util');
+function Girl(name){
+    this.name = name;
+    EventEmitter.call(this);
+}
+util.inherits(Girl,EventEmitter);
+
+var girl = new Girl();
+function Boy(name){
+    this.name = name;
+    this.say = function(thing){
+        console.log(thing);
+    }
+}
+var xiaoming = new Boy('小明');
+var xiaohua = new Boy('小花');
+//注册监听 事件 订阅
+/*girl.addListener('看',xiaoming.say);*/
+//注册监听 事件 订阅
+girl.on('看',xiaohua.say);
+//发射事件 发布
+/*girl.emit('看','钻石');
+girl.removeListener('看',xiaoming.say);*/
+//girl.removeAllListeners('看');
+girl.emit('看','项链');
+//如果发射多次事件，只会触发一次
+girl.once('饿了',function(bs){
+    console.log('吃饭'+bs);
+});
+girl.emit('饿了','你谁啊');
+/*girl.emit('饿了', '不认识你啊');
+girl.emit('饿了','我怎么觉得你是');*/
