9efef911aad45b8b9ba284b516e0ecb371cae834
lishengsong <2353142548@qq.com>
2018-07-16 17:33:19 +0800

    新增tcp、udp
    Signed-off-by: lishengsong <2353142548@qq.com>

--- code/stream/unshift.js
+++ code/stream/unshift.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var rs = fs.createReadStream('./request.txt');
+var rs = fs.createReadStream('./data/request.txt');
 //解析头部
 var StringDecoder = require('string_decoder').StringDecoder;
 function parseHeader(callback) {
@@ -9,7 +9,25 @@ function parseHeader(callback) {
     function onReadable() {
         var  chunk;
         while (null != (chunk = rs.read())) {
-            var
+            var str = decoder.write(chunk);
+            if (str.match(/\r\n\r\n/)) {
+                var splits = str.split(/\r\n\r\n/);
+                // shift() 方法用于把数组的第一个元素从其中删除，并返回第一个元素的值
+                headers += splits.shift();
+                rs.removeListener('readable', onReadable);
+                var remain = splits.join('\r\n\r\n');
+                var buf = new Buffer(remain, 'utf8');
+                if(buf.length) rs.unshift(buf);
+                callback(headers);
+            } else {
+                headers += str;
+            }
         }
     }
 }
+parseHeader(function (header) {
+    console.log(header);
+    rs.on('data', function (data) {
+        console.log(header);
+    })
+});
